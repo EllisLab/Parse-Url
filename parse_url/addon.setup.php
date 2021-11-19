df131a7fcc3481c5e9bb040039d26c36f8f1206c
andy <andy@packettide.com>
2021-11-19 11:17:19 -0500

    reviewed ee6 compatiblity

--- parse_url/addon.setup.php
+++ parse_url/addon.setup.php
@@ -1,11 +1,11 @@
 <?php
 
 return array(
-	'author'      => 'EllisLab',
-	'author_url'  => 'https://ellislab.com/',
+	'author'      => 'Packet Tide',
+	'author_url'  => 'https://packettide.com/',
 	'name'        => 'Parse URL',
 	'description' => 'Parses URL in a string and returns only certain parts',
-	'version'     => '2.0',
+	'version'     => '2.1',
 	'namespace'   => 'User\Addons\ParseUrl',
 	'settings_exist' => FALSE
 );
